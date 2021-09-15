7ff730ef2e8fdbf16146cc732d171e341df5b38d
Timothy Newman <84350177+TimothyJNewman@users.noreply.github.com>
2021-09-15 23:46:31 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c9702308.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
